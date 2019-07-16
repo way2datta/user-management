387367c828e4a1aa0cfc5caf51e27de85a22dc94
Akash Singhal <akash@technogise.com>
2019-07-16 10:58:03 +0530

    removing index route

--- sequalize-way/app.js
+++ sequalize-way/app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const indexRouter = require(__dirname+'/routes');
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.use('/api', indexRouter);
 
 module.exports = app;
