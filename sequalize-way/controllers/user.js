7bc0c87efc62777e1a281e2b22a631987f8f6c6a
Akash Singhal <akash.singhal67@yahoo.com>
2019-07-16 16:12:08 +0530

    extracted all route handlers related to user in user controller

--- sequalize-way/controllers/user.js
+++ sequalize-way/controllers/user.js
@@ -5,3 +5,42 @@ module.exports.findAllUsers = (req, res) => {
         res.send(users);
     });
 };
+
+module.exports.createNewUser = (req, res) => {
+    if (req.body.email === undefined) {
+        const errors = {
+            errors: { message: "Email is required.", field: "Email" }
+        };
+        res.status(400);
+        return res.send(errors);
+    }
+    models.User.create(req.body).then(user => {
+        res.status(201);
+        res.send(user);
+    });
+};
+
+
+module.exports.updateById = (req, res) => {
+    models.User.update(
+        { firstName: req.body.firstName },
+        { where: { id: req.params.id } }
+    ).then(() => {
+        res.sendStatus(204);
+    });
+}
+
+
+module.exports.deleteById = (req, res) => {
+    models.User.destroy({
+        where: { id: req.params.id }
+    }).then(() => {
+        res.sendStatus(204);
+    });
+}
+
+module.exports.getUserById = (req, res) => {
+    models.User.findOne({ where: { id: req.params.id } }).then(user => {
+        res.status(200).send(user);
+    });
+}
--- sequalize-way/routes/users.js
+++ sequalize-way/routes/users.js
@@ -3,44 +3,17 @@ const router = express.Router();
 const models = require(__dirname + "/../models");
 const userController = require(__dirname + "/../controllers/user")
 
-router.post("/", (req, res) => {
-  if (req.body.email === undefined) {
-    const errors = {
-      errors: { message: "Email is required.", field: "Email" }
-    };
-    res.status(400);
-    return res.send(errors);
-  }
-  models.User.create(req.body).then(user => {
-    res.status(201);
-    res.send(user);
-  });
-});
+router.post("/", );
 
 router.get("/", userController.findAllUsers);
+router.post("/", userController.createNewUser);
+router.put("/:id", userController.updateById);
+router.delete("/:id", userController.deleteById);
+router.get("/:id", userController.getUserById);
 
 
-router.get("/:id", (req, res) => {
-  models.User.findOne({ where: { id: req.params.id } }).then(user => {
-    res.status(200).send(user);
-  });
-});
 
-router.delete("/:id", (req, res) => {
-  models.User.destroy({
-    where: { id: req.params.id }
-  }).then(() => {
-    res.sendStatus(204);
-  });
-});
 
-router.put("/:id", (req, res) => {
-  models.User.update(
-    { firstName: req.body.firstName },
-    { where: { id: req.params.id } }
-  ).then(() => {
-    res.sendStatus(204);
-  });
-});
+
 
 module.exports = router;
