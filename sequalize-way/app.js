2a54828969cacadb7cab7345db594cb6cde87598
Dagttatray Kale <way2datta@gmail.com>
2019-07-17 15:24:19 +0530

    disabled query logging in test

--- sequalize-way/app.js
+++ sequalize-way/app.js
@@ -1,16 +1,21 @@
 // TODO: Remove afterwards
 import "@babel/polyfill";
-const express = require('express')
+import express from 'express'
+import apiRoutes from './routes';
+
 const app = express()
-const indexRouter = require(__dirname+'/routes');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => res.send("Hello World!"));
-app.use('/api', indexRouter);
+app.use('/api', apiRoutes);
 
+app.use(function (err, req, res, next) {
+    console.log(err.stack);
+    res.send('Something broke!')
+})
 
 module.exports = app;
 
