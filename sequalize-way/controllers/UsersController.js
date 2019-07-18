4601cfb64a14b3e28230e35bb40a8196436fb963
Dagttatray Kale <way2datta@gmail.com>
2019-07-18 19:00:32 +0530

    Cleanup, mocha executing on whole test directory

--- sequalize-way/controllers/UsersController.js
+++ sequalize-way/controllers/UsersController.js
@@ -3,7 +3,7 @@ import validateUser from "../validators/validateUser";
 import UserService from "./../services/UserService";
 
 export default class UsersController {
-constructor(userService) {
+  constructor(userService) {
     this.service = userService || new UserService();
   }
   create = async (req, res) => {
@@ -11,6 +11,7 @@ constructor(userService) {
     if (validationErrors) {
       return res.status(HttpStatus.BAD_REQUEST).send(validationErrors);
     }
+
     const newUser = await this.service.create(req.body);
     res.status(HttpStatus.CREATED);
     res.send(newUser);
@@ -39,7 +40,7 @@ constructor(userService) {
   getById = async (req, res) => {
     const existingUser = await this.service.getById(req.params.id);
     if (!existingUser) {
-      return res.sendStatus(HttpStatus.NOT_FOUND);
+      return res.status(HttpStatus.NOT_FOUND).send();
     }
     res.status(HttpStatus.OK).send(existingUser);
   };
--- sequalize-way/routes/index.js
+++ sequalize-way/routes/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const models = require(__dirname + "/../models");
 const userRouter = require(__dirname+"/users");
-
 
 router.use('/users', userRouter);
 
